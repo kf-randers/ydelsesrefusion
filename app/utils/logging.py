3424fee9954df9a11e1367a5c40a09bac9ca15e6
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-05-06 16:24:26 +0200

    logging

---
+++ app/utils/logging.py
@@ -0,0 +1,21 @@
+import sys
+import logging
+
+from utils.config import DEBUG
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    if DEBUG:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    logger.addHandler(get_handler())
+    return logger
+
+
+def get_handler():
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('[%(levelname)s]%(name)s:%(asctime)s - %(message)s', "%d-%m-%Y %H:%M:%S")
+    handler.setFormatter(formatter)
+    return handler
