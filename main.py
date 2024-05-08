20d699954ef21736791010d18e2b196f0b56d4e9
kf-randers <kfkr@randers.dk>
2024-05-08 12:15:38 +0200

    rettelser

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import base64
 import pysftp
 import fnmatch
+import numpy as np
+import openpyxl
 
 import pandas as pd
 
@@ -38,17 +40,56 @@ def list_all_files():
     sftp = pysftp.Connection(host=SFTP_HOST, username=SFTP_USER, private_key=key_path, private_key_pass=SSH_KEY_PASS, cnopts=cnopts)
 
     # filter away directorires and files without file extensions
-    filelist = [ f for f in sftp.listdir(REMOTE_DIR) if fnmatch.fnmatch(f, '*.csv') ]
+    filelist = [ f for f in sftp.listdir(REMOTE_DIR) if fnmatch.fnmatch(f, 'yr-ydelsesrefusion-beregning-730-0050-2024-03-03.csv') ]
     return filelist, sftp
 
-def habdle_files(files, connection):
+def handle_files(files, connection):
     for filename in files:
         # Open file
-        with connection.open(os.path.join(REMOTE_DIR, filename).replace("\\","/")) as f:
+
+        connection.get(os.path.join(REMOTE_DIR, filename).replace("\\","/"), "data/"+filename)
+
+        #with connection.open(os.path.join(REMOTE_DIR, filename).replace("\\","/"), filename) as f:
+            #print(os.path.join(REMOTE_DIR, filename).replace("\\","/"))
             # CSV file open as f, do something with it
             # E.g. read into pandas dataframe
-            df = pd.read_csv(f, sep=';', engine='python')
+            #df = pd.read_csv(f, sep=';', engine='python')
+            #print(df.head)  
+
+def data_wrangling(files):
+    for filename in files:
+        df=pd.read_csv("data/"+filename, sep=';', engine='python')
+
+        # Ydelsestyper
+        ydelsestyper=df[['Ydelse']].groupby(['Ydelse']).sum().reset_index()
+        #print(ydelsestyper.to_string(index=False))
+
+        # Antal ydelsesmodtagere 
+        # Beholder første observation pr. Uge x Ydelse x CPR nummer
+        df2=df[['Uge','CPR nummer','Ydelse']].groupby(['Uge','Ydelse','CPR nummer']).first().reset_index()
+        #print(len(df),len(df2))
+        #print(df2.head(10))
+
+        df3=df2.groupby(['Uge','Ydelse'])['CPR nummer'].count()
+        df4=df3.reset_index()
+
+        return df4
+
 
 
 file_list, conn = list_all_files()
-habdle_files(file_list, conn)
+print(file_list, len(file_list))
+
+#handle_files(file_list, conn)
+
+
+def data_export(df):
+    df.to_csv('data/yr-data.csv', index=False)
+
+
+
+file_list=['yr-ydelsesrefusion-beregning-730-0050-2024-03-03.csv']
+workdata=data_wrangling(file_list)
+
+data_export(workdata)
+        
